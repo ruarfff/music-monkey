86e70ebea51bde4fd2e197151795d2918c6cf3fd
Balyko Eugene <balyko.peaceit@gmail.com>
2018-12-17 13:38:17 +0200

    clear search results

--- packages/host/src/playlist/playlistReducer.ts
+++ packages/host/src/playlist/playlistReducer.ts
@@ -1,18 +1,19 @@
+import { LOCATION_CHANGE } from 'connected-react-router'
 import Action from '../IAction'
 import {
-  ADD_TRACK_FAILURE,
-  ADD_TRACK_SUCCESS,
-  FETCH_PLAYLISTS,
-  FETCH_PLAYLISTS_ERROR,
-  FETCH_PLAYLISTS_SUCCESS,
-  PLAYLIST_DESELECTED,
-  PLAYLIST_SELECTED,
-  REMOVE_TRACK_FAILURE,
-  REMOVE_TRACK_SUCCESS,
-  SEARCH_TRACKS_FAILURE,
-  SEARCH_TRACKS_SUCCESS,
-  TRACK_FEATURES_FAILURE,
-  TRACK_FEATURES_SUCCESS
+ADD_TRACK_FAILURE,
+ADD_TRACK_SUCCESS,
+FETCH_PLAYLISTS,
+FETCH_PLAYLISTS_ERROR,
+FETCH_PLAYLISTS_SUCCESS,
+PLAYLIST_DESELECTED,
+PLAYLIST_SELECTED,
+REMOVE_TRACK_FAILURE,
+REMOVE_TRACK_SUCCESS,
+SEARCH_TRACKS_FAILURE,
+SEARCH_TRACKS_SUCCESS,
+TRACK_FEATURES_FAILURE,
+TRACK_FEATURES_SUCCESS
 } from './playlistActions'
 import initialState from './playlistInitialState'
 
@@ -21,6 +22,11 @@ export default function playlists(
   { type, payload }: Action
 ) {
   switch (type) {
+    case LOCATION_CHANGE:
+      return {
+        ...state,
+        searchResult: {},
+      }
     case TRACK_FEATURES_SUCCESS:
       return {
         ...state,
