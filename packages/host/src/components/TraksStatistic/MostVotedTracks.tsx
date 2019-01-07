dd0e7d79d33ee441901dff2444d3e8cc805fbde2
Balyko Eugene <balyko.peaceit@gmail.com>
2019-01-07 13:11:22 +0200

    fix for most voted tracks

--- packages/host/src/components/TraksStatistic/MostVotedTracks.tsx
+++ packages/host/src/components/TraksStatistic/MostVotedTracks.tsx
@@ -1,6 +1,7 @@
 import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
 import Paper from '@material-ui/core/Paper'
+import { isEmpty } from 'lodash'
 import * as React from 'react'
 import IEvent from '../../event/IEvent'
 import IAction from '../../IAction'
@@ -28,7 +29,8 @@ class MostVotedTracks extends React.Component<IMostPopularTracksProps> {
 
   public componentWillReceiveProps(nextProps: IMostPopularTracksProps) {
     const { pickedEvent } = this.state
-    const { votes, events } = this.props
+    const { votes } = this.props
+
     if (nextProps.votes !== votes && pickedEvent.playlist) {
       this.props.sortPlaylistByVotesDescending(
         pickedEvent.playlist,
@@ -37,10 +39,10 @@ class MostVotedTracks extends React.Component<IMostPopularTracksProps> {
     } else if (
       nextProps.votes !== votes &&
       !pickedEvent.playlist &&
-      events[0].playlist
+      nextProps.events[0].playlist
     ) {
       this.props.sortPlaylistByVotesDescending(
-        events[0].playlist,
+        nextProps.events[0].playlist,
         this.props.votes
       )
     }
@@ -59,8 +61,13 @@ class MostVotedTracks extends React.Component<IMostPopularTracksProps> {
   }
 
   public render() {
-    const { anchorEl } = this.state
+    const { anchorEl, pickedEvent } = this.state
     const { events, playlist } = this.props
+
+    const eventsWithTracks = events.filter((event) =>
+      event.playlist && event.playlist.tracks.items.length > 0
+    )
+
     return (
       <Paper>
         <span className="title">Most Voted Tracks</span>
@@ -70,7 +77,7 @@ class MostVotedTracks extends React.Component<IMostPopularTracksProps> {
           className="menuBtn"
           onClick={this.handleClick}
         >
-          Pick Event
+          { !isEmpty(pickedEvent) ? pickedEvent.name : 'Pick Event' }
         </span>
         <Menu
           id="simple-menu"
@@ -78,8 +85,8 @@ class MostVotedTracks extends React.Component<IMostPopularTracksProps> {
           open={Boolean(anchorEl)}
           onClose={this.handleClose()}
         >
-          {events &&
-            events.map((event: IEvent, index: number) => {
+          {eventsWithTracks &&
+            eventsWithTracks.map((event: IEvent, index: number) => {
               if (event.eventId) {
                 return (
                   <MenuItem key={index} onClick={this.handleClose(event)}>
@@ -87,7 +94,7 @@ class MostVotedTracks extends React.Component<IMostPopularTracksProps> {
                   </MenuItem>
                 )
               }
-              return <MenuItem onClick={this.handleClose()}>no events</MenuItem>
+              return <MenuItem key={index} onClick={this.handleClose()}>no events</MenuItem>
             })}
         </Menu>
         <div className="listWrapper">
