d629c9a8944d693ac30d8accdbc99a1b73c7ff52
Ruairi O Brien <ruairitobrien@gmail.com>
2019-10-06 09:18:48 +0100

    Tidy up and update of theme

--- packages/host/src/rootState.ts
+++ packages/host/src/rootState.ts
@@ -2,9 +2,7 @@ import IAuthState from './auth/IAuthState'
 import IEventState from './event/IEventState'
 import IEventPlaylistState from './event/eventPlaylist/IEventPlaylistState'
 import IEventViewState from './event/eventView/IEventViewState'
-import { IHomeState } from './home/IHomeState'
 import { IInsightsInitialState } from './insights/insightsInitialState'
-import { INotificationState } from './notification/notificationInitialState'
 import IPlaylistState from './playlist/IPlaylistState'
 import IRecommendationsState from './recommendations/IRecommendationState'
 import ISuggestionState from './suggestion/ISuggestionState'
@@ -15,13 +13,12 @@ export default interface IRootState {
   auth: IAuthState
   user: IUserState
   playlist: IPlaylistState
-  home: IHomeState
   event: IEventState
   eventView: IEventViewState
   eventPlaylist: IEventPlaylistState
   suggestion: ISuggestionState
   vote: IVoteState
   insights: IInsightsInitialState
-  notification: INotificationState
+
   recommendation: IRecommendationsState
 }
