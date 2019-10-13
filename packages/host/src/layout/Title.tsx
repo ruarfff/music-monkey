b0a9e96b7b72965a41b0446b2a8addafa0a45f4f
Ruairi O Brien <ruairitobrien@gmail.com>
2019-10-13 13:15:44 +0100

    Fixed title rendering

--- packages/host/src/layout/Title.tsx
+++ packages/host/src/layout/Title.tsx
@@ -6,9 +6,12 @@ import './Title.scss'
 
 const pathToTitle = {
   '/create-event': 'Create Event',
-  '/all-events': 'Events',
-  '/upcoming-events': 'Upcoming Event',
-  '/past-events': 'Past Events',
+  '/catalogue/all-events': 'Events',
+  '/catalogue/upcoming-events': 'Upcoming Event',
+  '/catalogue/past-events': 'Past Events',
+  '/catalogue/all-playlists': 'Playlists',
+  '/catalogue/upcoming-playlists': 'Upcoming Playlists',
+  '/catalogue/past-playlists': 'Past Playlists',
   '/account': 'Account Settings',
   '/insights': 'Insights'
 }
@@ -20,7 +23,7 @@ interface ITitleProps extends RouteComponentProps {
 const Title = ({ location, event }: ITitleProps) => {
   const path = location.pathname
   let title = ''
-  if (event && path === `/events/${event.eventId}`) {
+  if (event && path.startsWith('/events/' + event.eventId)) {
     title = event.name
   } else {
     title = pathToTitle[path] || 'MusicMonkey'
