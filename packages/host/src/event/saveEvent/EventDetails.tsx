91b212ea83f81da2ec9260bfcc313839a56f9d39
Ruairi O Brien <ruairitobrien@gmail.com>
2019-12-02 23:03:01 +0000

    Added back organizer

--- packages/host/src/event/saveEvent/EventDetails.tsx
+++ packages/host/src/event/saveEvent/EventDetails.tsx
@@ -3,11 +3,15 @@ import { Grid } from '@material-ui/core'
 import { Field, FieldProps } from 'formik'
 import EventDateTimePicker from './EventDateTimePicker'
 import EventLocationInput from './EventLocationInput'
+import EventTextInput from './EventTextInput'
 import './EventDetails.scss'
 
 const EventDetails = () => {
   return (
     <Grid container className="EventDetails-root">
+      <Grid item={true} xs={12} sm={6}>
+        <EventTextInput name="organizer" label="Organizer" />
+      </Grid>
       <Grid item xs={12}>
         <Field name="startDateTime">
           {({ field: { value }, form: { setFieldValue } }: FieldProps) => {
