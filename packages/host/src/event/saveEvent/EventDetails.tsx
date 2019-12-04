7aab8a21d146acf4b5921ffe88a73e9572874367
Ruairi O Brien <ruairitobrien@gmail.com>
2019-12-04 22:13:51 +0000

    Settings to detaisl page

--- packages/host/src/event/saveEvent/EventDetails.tsx
+++ packages/host/src/event/saveEvent/EventDetails.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
-import { Grid } from '@material-ui/core'
+import { Grid, Typography } from '@material-ui/core'
 import { Field, FieldProps } from 'formik'
 import EventDateTimePicker from './EventDateTimePicker'
 import EventLocationInput from './EventLocationInput'
 import EventTextInput from './EventTextInput'
+import EventSettings from './EventSettings'
 import './EventDetails.scss'
 
 const EventDetails = () => {
   return (
     <Grid container className="EventDetails-root">
       <Grid item={true} xs={12} sm={6}>
+        <Typography
+          className="EventDetails-settings-title"
+          variant="h6"
+          align="center"
+          gutterBottom
+        >
+          Party settings
+        </Typography>
+        <EventSettings />
+      </Grid>
+      <Grid item={true} xs={12} sm={12}>
         <EventTextInput name="organizer" label="Organizer" />
       </Grid>
       <Grid item xs={12}>
