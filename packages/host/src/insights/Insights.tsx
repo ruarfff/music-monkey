3f3b8df9e777d6f456100fb96d0ffb7dd2e4c63a
Ruairi O Brien <ruairitobrien@gmail.com>
2020-02-12 00:14:31 +0000

    Made insights page look less terrible

--- packages/host/src/insights/Insights.tsx
+++ packages/host/src/insights/Insights.tsx
@@ -1,22 +1,17 @@
-import React from 'react'
+import React, { FC } from 'react'
 import Grid from '@material-ui/core/Grid'
-import { Typography } from '@material-ui/core'
 import EventSelect from 'event/select/EventSelectContainer'
 import PieChartWidget from './PieChart'
 import MostPopularTracks from 'insights/MostPopularTracks'
-import MostVotedTracks from 'insights/MostVotedTracks'
 import { Event } from 'mm-shared'
 import { Action, User, TrackVoteStatus, Playlist } from 'mm-shared'
 import './Insights.scss'
 
-interface IInsightsProps {
+interface InsightsProps {
   user: User
   event: Event
   events: Event[]
-  pickedEvent: string
   votes: Map<string, TrackVoteStatus>
-  fetchPlaylists(user: User): Action
-  filterByEventPick(id: any): Action
   sortPlaylistByVotesDescending(
     playlist: Playlist,
     votes: Map<string, TrackVoteStatus>
@@ -24,59 +19,27 @@ interface IInsightsProps {
   fetchEventVotes(eventId: string): Action
 }
 
-class Insights extends React.Component<IInsightsProps> {
-  public componentDidMount() {
-    if (this.props.user) {
-      this.props.fetchPlaylists(this.props.user)
-    }
-  }
-
-  public render() {
-    const {
-      event,
-      events,
-      pickedEvent,
-      votes,
-      filterByEventPick,
-      sortPlaylistByVotesDescending,
-      fetchEventVotes
-    } = this.props
-
-    return (
-      <Grid container spacing={2} className="Insights-root">
-        <Grid item xs={12}>
-          <EventSelect />
-        </Grid>
-        <Grid item xs={12} className="insightsContainer">
-          <Typography>Tracks Statistic</Typography>
-          <Grid container={true} spacing={3}>
-            <Grid item={true} md={6}>
-              <MostPopularTracks events={events} />
-            </Grid>
-            <Grid item={true} md={6}>
-              <MostVotedTracks
-                events={events}
-                votes={votes}
-                sortPlaylistByVotesDescending={sortPlaylistByVotesDescending}
-                fetchEventVotes={fetchEventVotes}
-                playlist={event.playlist!}
-              />
-            </Grid>
-          </Grid>
-          <Typography>Guests Statistic</Typography>
-          <Grid container={true} spacing={3}>
-            <Grid item={true} md={6}>
-              <PieChartWidget
-                filterByEventPick={filterByEventPick}
-                pickedEvent={pickedEvent}
-                events={events}
-              />
-            </Grid>
-          </Grid>
-        </Grid>
+const Insights: FC<InsightsProps> = ({
+  user,
+  event,
+  events,
+  votes,
+  sortPlaylistByVotesDescending,
+  fetchEventVotes
+}) => {
+  return (
+    <Grid container spacing={2} className="Insights-root">
+      <Grid item xs={12}>
+        <EventSelect />
       </Grid>
-    )
-  }
+      <Grid item={true} xs={12}>
+        <MostPopularTracks events={events} />
+      </Grid>
+      <Grid item={true} xs={12}>
+        <PieChartWidget event={event} events={events} />
+      </Grid>
+    </Grid>
+  )
 }
 
 export default Insights
--- packages/host/src/insights/PieChart.tsx
+++ packages/host/src/insights/PieChart.tsx
@@ -1,143 +1,74 @@
-import React from 'react'
-import Button from '@material-ui/core/Button'
-import Menu from '@material-ui/core/Menu'
-import MenuItem from '@material-ui/core/MenuItem'
+import React, { FC } from 'react'
 import Paper from '@material-ui/core/Paper'
-import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles'
 import Typography from '@material-ui/core/Typography'
 import flattenDeep from 'lodash/flattenDeep'
 import { Cell, Legend, Pie, PieChart, Tooltip } from 'recharts'
 import isEmpty from 'lodash/isEmpty'
-import { Event } from 'mm-shared'
-import { Action, Rsvp } from 'mm-shared'
+import { Event, Rsvp } from 'mm-shared'
 
-const decorate = withStyles(() => ({
-  title: {
-    fontSize: '20px',
-    padding: '10px'
-  }
-}))
-
-interface IPieChartWidgetProps {
+interface PieChartWidgetProps {
   events: Event[]
-  pickedEvent: string
-  filterByEventPick(id: any): Action
+  event: Event
 }
 
-class PieChartWidget extends React.Component<
-  IPieChartWidgetProps & WithStyles
-> {
-  public state = {
-    anchorEl: null,
-    pickedEventName: 'All'
-  }
+const PieChartWidget: FC<PieChartWidgetProps> = ({ events }) => {
+  const COLORS = ['#0088FE', '#00C49F', '#FF8042', '#FFBB28']
+  const data = guestsPieData(events)
 
-  public handleClick = (event: any) => {
-    this.setState({ anchorEl: event.currentTarget })
-  }
-
-  public handleClose = (event: Event) => () => {
-    this.setState({
-      anchorEl: null,
-      pickedEventName: event.name || event.eventId
-    })
-    this.props.filterByEventPick(event.eventId)
-  }
-
-  public render() {
-    const { anchorEl } = this.state
-    const COLORS = ['#0088FE', '#00C49F', '#FF8042', '#FFBB28']
-    const { events, pickedEvent, classes } = this.props
-    const data = this.guestsPieData(pickedEvent)
-
-    return (
-      <Paper>
-        <div>
-          <Button
-            aria-owns={anchorEl ? 'simple-menu' : undefined}
-            aria-haspopup="true"
-            onClick={this.handleClick}
-          >
-            Event: {this.state.pickedEventName}
-          </Button>
-          <Menu
-            id="simple-menu"
-            anchorEl={anchorEl}
-            open={Boolean(anchorEl)}
-            onClose={this.handleClose({ eventId: 'all' } as Event)}
-          >
-            <MenuItem onClick={this.handleClose({ eventId: 'all' } as Event)}>
-              All
-            </MenuItem>
-            {events.map(
-              (event, i) =>
-                event.eventId && (
-                  <MenuItem key={i} onClick={this.handleClose(event)}>
-                    {event.name}
-                  </MenuItem>
-                )
-            )}
-          </Menu>
-        </div>
-        <Typography className={classes.title}>Guest statistic</Typography>
-        <PieChart width={500} height={250}>
-          <Pie data={data} dataKey={'value'} innerRadius={60} outerRadius={80}>
-            {data.map((entry: any, index: number) => (
-              <Cell key={index} fill={COLORS[index % COLORS.length]} />
-            ))}
-          </Pie>
-          <Legend verticalAlign="bottom" align="center" height={36} />
-          <Tooltip />
-        </PieChart>
-      </Paper>
-    )
-  }
-
-  private guestsPieData = (eventId: string) => {
-    const { events } = this.props
-    const selectedEvent =
-      eventId !== 'all'
-        ? events.filter(event => event.eventId === eventId)
-        : events
-
-    const allGuests = flattenDeep<Rsvp>(
-      selectedEvent
-        .filter(event => !isEmpty(event.guests))
-        .map(event => (event.guests || []).map(guest => guest.rsvp))
-    )
-
-    const pendingGuest = allGuests.filter(
-      (guest: Rsvp | undefined) => guest && guest.status === 'Pending'
-    )
-    const goingGuest = allGuests.filter(
-      (guest: Rsvp | undefined) => guest && guest.status === "I'm Going"
-    )
-    const notGoingGuest = allGuests.filter(
-      (guest: Rsvp | undefined) => guest && guest.status === "I'm not going"
-    )
-    const maybeGuest = allGuests.filter(
-      (guest: Rsvp | undefined) => guest && guest.status === 'Maybe'
-    )
-
-    return [
-      {
-        value: pendingGuest.length,
-        name: 'Pending'
-      },
-      {
-        value: goingGuest.length,
-        name: 'Going'
-      },
-      {
-        value: notGoingGuest.length,
-        name: 'Not going'
-      },
-      {
-        value: maybeGuest.length,
-        name: 'Maybe'
-      }
-    ]
-  }
+  return (
+    <Paper>
+      <Typography>Guest statistic</Typography>
+      <PieChart width={300} height={250}>
+        <Pie data={data} dataKey={'value'} innerRadius={60} outerRadius={80}>
+          {data.map((entry: any, index: number) => (
+            <Cell key={index} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Legend verticalAlign="bottom" align="center" height={36} />
+        <Tooltip />
+      </PieChart>
+    </Paper>
+  )
 }
 
-export default decorate(PieChartWidget)
+function guestsPieData(events: Event[]) {
+  const allGuests = flattenDeep<Rsvp>(
+    events
+      .filter(event => !isEmpty(event.guests))
+      .map(event => (event.guests || []).map(guest => guest.rsvp))
+  )
+
+  const pendingGuest = allGuests.filter(
+    (guest: Rsvp | undefined) => guest && guest.status === 'Pending'
+  )
+  const goingGuest = allGuests.filter(
+    (guest: Rsvp | undefined) => guest && guest.status === "I'm Going"
+  )
+  const notGoingGuest = allGuests.filter(
+    (guest: Rsvp | undefined) => guest && guest.status === "I'm not going"
+  )
+  const maybeGuest = allGuests.filter(
+    (guest: Rsvp | undefined) => guest && guest.status === 'Maybe'
+  )
+
+  return [
+    {
+      value: pendingGuest.length,
+      name: 'Pending'
+    },
+    {
+      value: goingGuest.length,
+      name: 'Going'
+    },
+    {
+      value: notGoingGuest.length,
+      name: 'Not going'
+    },
+    {
+      value: maybeGuest.length,
+      name: 'Maybe'
+    }
+  ]
+}
+
+export default PieChartWidget
